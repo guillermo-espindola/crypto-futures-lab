921e3238b314f52ec7145babf62ab50a3b988ed4
Guillermo.Espindola <espindolazga@gmail.com>
2026-06-04 12:20:51 -0400

    REFACTOR: Directory and filename for logs

--- Horus/utils/logger_file.py
+++ Horus/utils/logger_file.py
@@ -1,15 +1,17 @@
+import os
 import logging
 from typing import Any
 from utils.logger_interface import ILogger
 
 class LoggerFile(ILogger):
     def __init__(self, type: Any):
-        self.name = type.__name__ if hasattr(type, '__name__') else str(type)
-        self.logger = logging.getLogger(self.name)
-        self.logger.setLevel(logging.INFO)
-        
-        if not self.logger.handlers:
-            # 1. Definir el formato del mensaje
+        self._name = type.__name__ if hasattr(type, '__name__') else str(type)
+        self._logger = logging.getLogger(self._name)
+        self._logger.setLevel(logging.INFO)
+
+        if not self._logger.handlers:
+            os.makedirs("./logs", exist_ok=True)
+
             formatter = logging.Formatter(
                 "[%(asctime)s]"
                 "[%(levelname)s]"
@@ -17,15 +19,17 @@ class LoggerFile(ILogger):
                 "%(message)s"
             )
 
-            # 2. Configurar el manejador exclusivo para el archivo
-            file_handler = logging.FileHandler('horus.log', mode='a', encoding='utf-8')
+            file_handler = logging.FileHandler(
+                f"./logs/{self._name}.log",
+                mode="a",
+                encoding="utf-8"
+            )
             file_handler.setFormatter(formatter)
-            
-            # 3. Añadir solo el manejador de archivo al logger
-            self.logger.addHandler(file_handler)
+
+            self._logger.addHandler(file_handler)
 
     def info(self, message: str):
-        self.logger.info(message)
-    
+        self._logger.info(message)
+
     def error(self, message: str):
-        self.logger.error(message)
+        self._logger.error(message)
